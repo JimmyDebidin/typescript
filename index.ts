30661a41fba89966d90aed2021d30895bc942c72
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-03-21 16:05:48 +0100

    finish challenge 1

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
-"use strict";
-function hello(name) {
+function hello(name: string) {
   console.log("Hello " + name);
 }
+
 var firstName = "bob";
+
 hello(firstName);
 hello(firstName + " marley");
-function concat(a, b) {
+
+function concat(a: string, b: string): string {
   return a + b;
 }
+
 var ds = concat("Donkey", concat("School", "!"));
 console.log(ds);
